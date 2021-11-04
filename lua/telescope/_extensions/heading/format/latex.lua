4e7974b93f98000d9107335e5dd90554d6991d4c
Derick Tseng <derickboss1@gmail.com>
2021-11-04 04:37:27 -0700

    performance improvements for latex heading detection

--- lua/telescope/_extensions/heading/format/latex.lua
+++ lua/telescope/_extensions/heading/format/latex.lua
@@ -1,15 +1,22 @@
 local Latex = {}
 
-local function skipSyntax(lnum, col)
+local disallowedEnvs = {
+    'texMath',
+    'texComment'
+}
+
+local function isDisallowedEnv(lnum, col)
     local syntaxstack = vim.fn.synstack(lnum, col)
     local name
 
     for _, syntax in ipairs(syntaxstack) do
         name = vim.fn.synIDattr(syntax, 'name')
 
-        -- disallow math sections
-        if string.find(name, '^texMath') then
-            return true
+        -- disallowed environments
+        for _, env in ipairs(disallowedEnvs) do
+            if string.match(name, '^' .. env) then
+                return true
+            end
         end
     end
 
@@ -19,48 +26,44 @@ end
 function Latex.get_headings(filepath, start, total)
     local headings = {}
     local index = start
+    local matchtable = {}
     local matches = {
-        '\\part',
-        '\\chapter',
-        '\\section',
-        '\\subsection',
-        '\\subsubsection',
-        '\\paragraph',
-        '\\subparagraph',
+        'part',
+        'chapter',
+        'section',
+        'subsection',
+        'subsubsection',
+        'paragraph',
+        'subparagraph',
     }
+
+    -- allows syntax of matchtable[headingname]
+    for _, m in ipairs(matches) do
+        matchtable[m] = ''
+    end
 
     while index <= total do
         local line = vim.fn.getline(index)
         local trimmedline = string.gsub(line, '^%s+', '')
-        local skip
+        local headingname = string.match(trimmedline, '^\\%l+') or ''
+        headingname = string.sub(headingname, 2)
+        local skip = false
 
-        -- checks if line begins with a \\ (for performance reasons).
-        -- if it does begin with \\, we need to check the syntax stack.
-        -- otherwise, it will never match, so we can skip entirely
-        if string.find(trimmedline, '^\\') ~= nil then
+        if matchtable[headingname] then
             local firstWordCol = string.len(line) - string.len(trimmedline) + 1
-            skip = skipSyntax(index, firstWordCol)
+            skip = isDisallowedEnv(index, firstWordCol)
         else
             skip = true
         end
 
-        -- goes to next line
-        if skip then
-            goto next
+        if not skip then
+            table.insert(headings, {
+                heading = vim.trim(line),
+                line = index,
+                path = filepath,
+            })
         end
 
-        for _, pattern in pairs(matches) do
-            if string.find(trimmedline, '^' .. pattern) ~= nil then
-                table.insert(headings, {
-                    heading = vim.trim(line),
-                    line = index,
-                    path = filepath,
-                })
-                break
-            end
-        end
-
-        ::next::
         index = index + 1
     end
 

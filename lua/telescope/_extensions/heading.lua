fe50c022108f6024dffe09b7cb9c828eff0de8a5
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-18 14:25:52 +0800

    refine: vim.notify user when on unsupported filetype

--- lua/telescope/_extensions/heading.lua
+++ lua/telescope/_extensions/heading.lua
@@ -34,7 +34,7 @@ local function get_headings()
     )
     local ok, mod = pcall(require, mod_path)
     if not ok then
-        error('The file type is not supported by telescope-heading')
+        vim.notify('The file type is not supported by telescope-heading', vim.log.levels.WARN, { title = 'Telescope heading' })
         return nil
     end
 

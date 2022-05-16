4ac8d9c56626ef3da6e85e84b16c1c784a3c90a9
Jakob Hansen <jakob.hansen@hotmail.no>
2022-05-17 01:04:32 +0200

    feat: Add support for treesitter queries in norg filetype

--- lua/telescope/_extensions/heading/format/norg.lua
+++ lua/telescope/_extensions/heading/format/norg.lua
@@ -31,4 +31,36 @@ function Neorg.get_headings(filepath, start, total)
     return headings
 end
 
+function Neorg.ts_get_headings(filepath, bufnr)
+    local ts = vim.treesitter
+    local query = [[
+    [
+    (heading1) 
+    (heading2)
+    (heading3)
+    (heading4)
+    (heading5)
+    (heading6)
+    ]
+
+    @heading
+    ]]
+    local parsed_query = ts.parse_query('norg', query)
+    local parser = ts.get_parser(bufnr, 'norg')
+    local root = parser:parse()[1]:root()
+    local start_row, _, end_row, _ = root:range()
+
+    local headings = {}
+    for _, node in parsed_query:iter_captures(root, bufnr, start_row, end_row) do
+        local row, _ = node:range()
+        local line = vim.fn.getline(row + 1)
+        table.insert(headings, {
+            heading = vim.trim(line),
+            line = row + 1,
+            path = filepath,
+        })
+    end
+    return headings
+end
+
 return Neorg
